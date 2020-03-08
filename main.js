2e4ee0e6ef21b812e1ceda8fcd12f880ba03c247
doncity-tech <princewill.opara@outlook.com>
2020-03-08 15:39:15 +0100

    Good performance

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Star rating app built by Opara Princewill
 //Basic functions
 const qs = (elem) => {
 	return document.querySelector(elem);
@@ -6,66 +7,66 @@ const qsAll = (elem) => {
 	return document.querySelectorAll(elem);
 }
 
+const allStar = ['rate-1','rate-2','rate-3','rate-4','rate-5'];
+
 //Add Star Rating
 let getStar = -1;
-qs('#rating').addEventListener('mouseover', (e) => {
-	const renderRate = (num) => {
-		let allStar = ['rate-1','rate-2','rate-3','rate-4','rate-5'];
-		for(let i = 0; i < num; i++) {
-			qs(`#${allStar[i]}`).style.color = "orange";
-		}
+
+const correntRate = (num) => {
+	allStar.forEach((x) => {
+		qs(`#${x}`).style.color = "#d6d6d6";
+	});
+
+	for(let i = 0; i < num; i++) {
+		qs(`#${allStar[i]}`).style.color = "orange";
 	}
+}
 
-	if(e.target.nodeName === 'I') {
-		let id = e.target.getAttribute('id').split('-')[1];
-		renderRate(id);
+const renderRate = (num) => {
+	for(let i = 0; i < num; i++) {
+		qs(`#${allStar[i]}`).style.color = "orange";
 	}
-});
+}
 
-qs('#rating').addEventListener('click', (e) => {	
-	const correntRate = (num) => {		
-		let allStar = ['rate-1','rate-2','rate-3','rate-4','rate-5'];
-		allStar.forEach((x) => {
-			qs(`#${x}`).style.color = "#d6d6d6";
-		});
-
-		for(let i = 0; i < num; i++) {
-			qs(`#${allStar[i]}`).style.color = "orange";
-		}
-	}
-
-	if(e.target.nodeName === 'I') {
-		let id = e.target.getAttribute('id').split('-')[1];
-        getStar = id;
-        qs('#s-value').textContent = id;
-		correntRate(id);		
-	}
-});
-
+const removeRate = (num) => {
+	for(let i = num - 1; i > -1; i--) {								
+		if(getStar <= i) {
+			qs(`#${allStar[i]}`).style.color = "#d6d6d6";
+		}				
+	}			
+}
 
 (() => {
-	let allStar = ['rate-1','rate-2','rate-3','rate-4','rate-5'];
-	const removeRate = (num) => {
-		for(let i = num - 1; i > -1; i--) {								
-			if(getStar <= i) {
-				qs(`#${allStar[i]}`).style.color = "#d6d6d6";
-			}				
-		}			
-	}
-	
-	for(let i = 0; i < 5; i++) {
-		qs(`#${allStar[i]}`).addEventListener('mouseleave', (e) => {
+	allStar.forEach((x) => {
+		qs(`#${x}`).addEventListener('mouseover', (e) => {
+			let id = e.target.getAttribute('id').split('-')[1];	
+			renderRate(id);
+		});
+	});
+
+	allStar.forEach((x) => {
+		qs(`#${x}`).addEventListener('mouseleave', (e) => {
 			let id = e.target.getAttribute('id').split('-')[1];	
 			removeRate(id);
 		});
-	}
+	});
+
+	allStar.forEach((x) => {
+		qs(`#${x}`).addEventListener('click', (e) => {
+			let id = e.target.getAttribute('id').split('-')[1];
+			let rateValue = e.target.nextSibling.textContent;
+			getStar = id;
+			qs('#s-value').textContent = `= ${id} ${rateValue}`;
+			correntRate(id);	
+		});
+	});
+	
+	qs('.clear-btn').addEventListener('click', () => {
+		allStar.forEach((x) => {
+			qs(`#${x}`).style.color = "#d6d6d6";
+		});
+		getStar = -1;
+		qs('#s-value').textContent = "";
+	})
 })();
 
-qs('.clear-btn').addEventListener('click', () => {
-    let allStar = ['rate-1','rate-2','rate-3','rate-4','rate-5'];
-    allStar.forEach((x) => {
-        qs(`#${x}`).style.color = "#d6d6d6";
-    });
-    getStar = -1;
-    qs('#s-value').textContent = "";
-})
